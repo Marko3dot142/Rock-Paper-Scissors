490bf6d0e0fd8839beb07dfc09b21e1f115dcf8a
Marko3dot142 <devtasks01@gmail.com>
2025-04-04 22:45:06 +0800

    Update rps.js

--- rps.js
+++ rps.js
@@ -1,16 +1,67 @@
-document.getElementById('rock').onclick = function () {
+/**document.getElementById('rock').onclick = function () {
+  const randomNumber = Math.random ();
+  if (randomNumber <= 0.3333) {
+    alert('You picked rock. Computer picked scissors. You win.');
+  } else if (randomNumber > 0.3333 && randomNumber<= 0.6666) {
+    alert(`You picked rock. Computer picked rock. It's a tie.`);
+  } else {
+    alert('You picked rock. Computer picked paper. You lose.');
+  }
+};
 
+document.getElementById('paper').onclick = function () {
+  const randomNumber = Math.random ();
+  if (randomNumber <= 0.3333) {
+    alert('You picked paper. Computer picked rock. You win.');
+  } else if (randomNumber > 0.3333 && randomNumber<= 0.6666) {
+    alert(`You picked paper. Computer picked paper. It's a tie.`);
+  } else {
+    alert('You picked paper. Computer picked scissors. You lose.');
+  }
+};
+
+document.getElementById('scissors').onclick = function () {
+  const randomNumber = Math.random ();
+  if (randomNumber <= 0.3333) {
+    alert('You picked scissors. Computer picked paper. You win.');
+  } else if (randomNumber > 0.3333 && randomNumber<= 0.6666) {
+    alert(`You picked scissors. Computer picked scissors. It's a tie.`);
+  } else {
+    alert('You picked scissors. Computer picked rock. You lose.');
+  }
+}; */
+
+/**Math.random will let computer choose n >= 0 or <1. */
+/**Here, we evenly divided the range of n into three parts that are fit with 3 possibilities of the game.*/
+/**We can divide the range to the number of part base on situation (if 4 outcomes, then 0-0.25, 0.25-0.5...) */
+/** &&: Stands for "and", use to declare two outcomes as one condition. */
+/** && (Logical AND): Evaluates to true only if **both** conditions are true. */
+/** ||: Stands for "or", use to declare either one outcome as one condition.*/
+/** || (Logical OR): Evaluates to true if **at least one** condition is true.*/
+
+
+/**More tidy code for rps by using function. */
+
+let computerMove = ''; /**Variable: computerMove need to be defined first. Cannot create inside the function because any variables in the scope only exist in the scope.*/
+
+function pickComputerMove() {
+  
   const randomNumber = Math.random ();   /**Step 1: Generate a new random number*/
 
-  let computerMove = ''; /**Step 2: Determine computer's move */
-
   if (randomNumber < 1/3) {
-  computerMove = 'rock';
+      computerMove = 'rock';
   } else if (randomNumber >= 1/3 && randomNumber <2/3) {
-  computerMove = 'paper';
+      computerMove = 'paper';
   } else {
-  computerMove = 'scissors';
+      computerMove = 'scissors';
   };
+
+  return 'rock';
+}
+
+document.getElementById('rock').onclick = function () {
+
+  console.log(pickComputerMove());
 
   let result = ''; /**Step 3: Determine the result based on player's choice */
 
@@ -27,17 +78,7 @@ document.getElementById('rock').onclick = function () {
 
 document.getElementById('paper').onclick = function () {
 
-  const randomNumber = Math.random ();   /**Step 1: Generate a new random number*/
-  
-  let computerMove = ''; /**Step 2: Determine computer's move */
-  
-  if (randomNumber < 1/3) {
-    computerMove = 'rock';
-  } else if (randomNumber >= 1/3 && randomNumber <2/3) {
-    computerMove = 'paper';
-  } else {
-    computerMove = 'scissors';
-  };
+  pickComputerMove();
   
   let result = ''; /**Step 3: Determine the result based on player's choice */
   
@@ -49,22 +90,12 @@ document.getElementById('paper').onclick = function () {
     } else {
       result = 'You win.';
     }
-    alert(`You picked rock. Computer picked ${computerMove}. ${result}`); /**Step 4: Display the result */
+    alert(`You picked paper. Computer picked ${computerMove}. ${result}`); /**Step 4: Display the result */
 };
 
 document.getElementById('scissors').onclick = function () {
 
-  const randomNumber = Math.random ();   /**Step 1: Generate a new random number*/
-  
-  let computerMove = ''; /**Step 2: Determine computer's move */
-  
-  if (randomNumber < 1/3) {
-    computerMove = 'rock';
-  } else if (randomNumber >= 1/3 && randomNumber <2/3) {
-    computerMove = 'paper';
-  } else {
-    computerMove = 'scissors';
-  };
+  pickComputerMove();
   
   let result = ''; /**Step 3: Determine the result based on player's choice */
   
@@ -76,6 +107,6 @@ document.getElementById('scissors').onclick = function () {
     } else {
       result = 'You win.';
     }
-    alert(`You picked rock. Computer picked ${computerMove}. ${result}`); /**Step 4: Display the result */
+    alert(`You picked scissors. Computer picked ${computerMove}. ${result}`); /**Step 4: Display the result */
   };
 
